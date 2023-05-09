552f94176d692f5812b0c26c744b586620dcb146
sofoyan gevork <aab81@bk.ru>
2023-05-09 16:39:59 +0300

    ,,,

--- js/counter.js
+++ js/counter.js
@@ -3,33 +3,22 @@
 //Добавляем прослушку на всем окне
 window.addEventListener('click', function(event)
 {
-    console.log('click window');
-/*
-    //получам элемент по которому кликнули
-    console.log(event.target);
 
-    //получам элемент помеченный data-action, то есть либо плюс либо минус
-    console.log(event.target.dataset.action);
-*/  
+    //находим ближайшего родителя с классом .counter-wrapper (обертку счетчика)
+    const counterWrapper = event.target.closest('.counter-wrapper');
+    
+    //находим в родителе элемент со счетчиком
+    const counter = counterWrapper.querySelector('[data-counter]');
 
     //Если нажатая кнопка '-'
     if(event.target.dataset.action === 'plus')
     {
-        //находим ближайшего родителя с классом .counter-wrapper (обертку счетчика)
-        const counterWrapper = event.target.closest('.counter-wrapper');
-
-        //находим в родителе элемент со счетчиком
-        const counter = counterWrapper.querySelector('[data-counter]');
-
         ++counter.innerText;
     }
 
     //Если нажатая кнопка '+'
     if(event.target.dataset.action === 'minus')
     {
-        const counterWrapper = event.target.closest('.counter-wrapper');
-
-        const counter = counterWrapper.querySelector('[data-counter]');
 
         if(parseInt(counter.innerText)>1)
         {
